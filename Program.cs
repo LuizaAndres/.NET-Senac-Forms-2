839c82428d6410e55a5473950744a293b42035ae
LuizaAndres <55116236+LuizaAndres@users.noreply.github.com>
2020-05-14 16:53:11 -0300

    fix indentation and spaces

--- Program.cs
+++ Program.cs
@@ -16,69 +16,67 @@ namespace _NET_Senac_Forms_2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Formulario());
-
         }
-            public class Formulario : Form{
-                Label lbNome;
-                TextBox tbNome;
-                Label lbDtNascimento;
-                MaskedTextBox mtbDtNascimento;
-                Label lbDiasDev;
-                NumericUpDown nudDiasDev;
-                PictureBox QRCode;
-                public Formulario(){
+        public class Formulario : Form{
+            Label lbNome;
+            TextBox tbNome;
+            Label lbDtNascimento;
+            MaskedTextBox mtbDtNascimento;
+            Label lbDiasDev;
+            NumericUpDown nudDiasDev;
+            PictureBox QRCode;
+            public Formulario(){
 
-                    this.Text = "Exercícios";
-                    this.Size = new Size(300,300);
-                    this.MinimizeBox = false;
-                    this.FormBorderStyle = FormBorderStyle.FixedSingle;
-                    this.StartPosition = FormStartPosition.CenterScreen;
+                this.Text = "Exercícios";
+                this.Size = new Size(300,300);
+                this.MinimizeBox = false;
+                this.FormBorderStyle = FormBorderStyle.FixedSingle;
+                this.StartPosition = FormStartPosition.CenterScreen;
 
-                    lbNome = new Label();
-                    lbNome.Text = "Nome";
-                    lbNome.Size = new Size(40,20);
-                    lbNome.Location = new Point(20, 20);
+                lbNome = new Label();
+                lbNome.Text = "Nome";
+                lbNome.Size = new Size(40,20);
+                lbNome.Location = new Point(20, 20);
 
-                    tbNome = new TextBox();
-                    tbNome.Size = new Size(150, 10);
-                    tbNome.Location = new Point(20, 40);
-                   
-                    lbDtNascimento = new Label();
-                    lbDtNascimento.Text = "Data de nascimento";
-                    lbDtNascimento.Size = new Size(150,20);
-                    lbDtNascimento.Location = new Point(20, 70);
+                tbNome = new TextBox();
+                tbNome.Size = new Size(150, 10);
+                tbNome.Location = new Point(20, 40);
 
-                    mtbDtNascimento = new MaskedTextBox();
-                    mtbDtNascimento.Mask = "00/00/0000";
-                    mtbDtNascimento.Size = new Size(150, 10);
-                    mtbDtNascimento.Location = new Point(20, 90);
+                lbDtNascimento = new Label();
+                lbDtNascimento.Text = "Data de nascimento";
+                lbDtNascimento.Size = new Size(150,20);
+                lbDtNascimento.Location = new Point(20, 70);
 
-                    lbDiasDev = new Label();
-                    lbDiasDev.Text = "Dias para devolução";
-                    lbDiasDev.Size = new Size(150, 20);
-                    lbDiasDev.Location = new Point(20, 120);
+                mtbDtNascimento = new MaskedTextBox();
+                mtbDtNascimento.Mask = "00/00/0000";
+                mtbDtNascimento.Size = new Size(150, 10);
+                mtbDtNascimento.Location = new Point(20, 90);
 
-                    nudDiasDev = new NumericUpDown();
-                    nudDiasDev.Minimum = 5;
-                    nudDiasDev.Maximum = 15;
-                    nudDiasDev.Size = new Size(90, 10)                    ;
-                    nudDiasDev.Location = new Point(20, 140);
-                    
-                    QRCode = new PictureBox();
-                    QRCode.SizeMode = PictureBoxSizeMode.StretchImage;
-                    QRCode.ClientSize = new Size(30,30);
-                    QRCode.Load("image.png");
-                    QRCode.Location = new Point(20, 170);
+                lbDiasDev = new Label();
+                lbDiasDev.Text = "Dias para devolução";
+                lbDiasDev.Size = new Size(150, 20);
+                lbDiasDev.Location = new Point(20, 120);
 
+                nudDiasDev = new NumericUpDown();
+                nudDiasDev.Minimum = 5;
+                nudDiasDev.Maximum = 15;
+                nudDiasDev.Size = new Size(90, 10)                    ;
+                nudDiasDev.Location = new Point(20, 140);
 
-                    this.Controls.Add(lbNome);
-                    this.Controls.Add(tbNome);
-                    this.Controls.Add(lbDtNascimento);
-                    this.Controls.Add(mtbDtNascimento);
-                    this.Controls.Add(lbDiasDev);
-                    this.Controls.Add(nudDiasDev);
-                    this.Controls.Add(QRCode);
-                }
+                QRCode = new PictureBox();
+                QRCode.SizeMode = PictureBoxSizeMode.StretchImage;
+                QRCode.ClientSize = new Size(30,30);
+                QRCode.Load("image.png");
+                QRCode.Location = new Point(20, 170);
+
+                this.Controls.Add(lbNome);
+                this.Controls.Add(tbNome);
+                this.Controls.Add(lbDtNascimento);
+                this.Controls.Add(mtbDtNascimento);
+                this.Controls.Add(lbDiasDev);
+                this.Controls.Add(nudDiasDev);
+                this.Controls.Add(QRCode);
             }
+        }
     }
 }
